8cb64ef42f29137b4482f2f92a2b7fc9251d3642
Riyad-07 <samimsamim1998@gmail.com>
2024-12-17 17:57:31 +0600

    update

--- src/components/Membar.jsx
+++ src/components/Membar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaArrowLeft, FaArrowRight, FaStar } from "react-icons/fa";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-import s1 from '../../public/assets/slider/s1.png'
+import s1 from "../../public/assets/slider/s1.png";
 
 const Membar = () => {
   var settings = {
@@ -51,8 +51,46 @@ const Membar = () => {
         Hear from women who share your passion for exploring new cultures,
         experiencing life abroad, and making the most of every opportunity.
       </p>
-                              {/* Slider */}
+      {/* Slider */}
       <Slider {...settings}>
+        <div className="w-full xl:w-[500px]">
+          <div className="flex items-center w-full xl:justify-between gap-x-10">
+            <div className="xl:w-[444px] w-full bg-[#0872BA] rounded-2xl p-5 xl:p-10 xl:pr-20">
+              <div className="flex items-center justify-between">
+                <div className="w-[88px] h-[88px] rounded-full bg-white">
+                  <img
+                    className="object-contain w-full h-full rounded-full"
+                    src={s1}
+                    alt="Sarah"
+                  />
+                </div>
+                <div className="flex items-center text-white gap-x-2">
+                  <FaStar />
+                  <p>5.0</p>
+                </div>
+              </div>
+              <h3 className="my-3 text-2xl font-semibold text-white font-monts">
+                Sarah Thompson
+              </h3>
+              <div className="flex flex-col gap-3">
+                <p className="font-monts text-lg text-[#F9FAFB]">
+                  Teacher • Non-smoker • Married
+                </p>
+                <p className="font-monts text-lg text-[#F9FAFB]">
+                  Silom, Bangkok
+                </p>
+                <p className="font-monts text-lg text-[#F9FAFB]">Dec 3-8</p>
+              </div>
+            </div>
+            <div className="w-auto">
+              <p className="md:text-xl sm:text-[14px] xl:text-2xl font-medium font-monts leading-[164%] text-[#344054] xl:w-[597px] w-full">
+                Our smart developers build innovative, efficient, and high-
+                performance solutions. efficient, and high- performance
+                solutions. efficient, and high- performance solutions.
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="w-full xl:w-[500px]">
           <div className="flex items-center w-full xl:justify-between gap-x-10">
             <div className="xl:w-[444px] w-full bg-[#0872BA] rounded-2xl p-5 xl:p-10 xl:pr-20">
@@ -81,37 +119,6 @@ const Membar = () => {
             </div>
           </div>
         </div>
-        <div className="w-full xl:w-[500px]">
-          <div className="flex items-center w-full xl:justify-between gap-x-10">
-            <div className="xl:w-[444px] w-full bg-[#0872BA] rounded-2xl p-5 xl:p-10 xl:pr-20">
-              <div className="flex items-center justify-between">
-                <div className="w-[88px] h-[88px] rounded-full bg-white">
-                  <img className="object-contain w-full h-full rounded-full" src={s1} alt="Sarah" />
-                </div>
-                <div className="flex items-center text-white gap-x-2">
-                  <FaStar />
-                  <p>5.0</p>
-                </div>
-              </div>
-              <h3 className="my-3 text-2xl font-semibold text-white font-monts">
-                Sarah Thompson
-              </h3>
-              <div className="flex flex-col gap-3">
-                <p className="font-monts text-lg text-[#F9FAFB]">Teacher • Non-smoker • Married</p>
-                <p className="font-monts text-lg text-[#F9FAFB]">Silom, Bangkok</p>
-                <p className="font-monts text-lg text-[#F9FAFB]">Dec 3-8</p>
-              </div>
-            </div>
-            <div className="w-auto">
-              <p className="md:text-2xl text-[18px] font-medium font-monts leading-[164%] text-[#344054] xl:w-[597px] w-full">
-              Our smart developers build innovative, efficient, and high- performance solutions. efficient, and high- performance solutions. efficient, and high- performance solutions.
-              </p>
-            </div>
-          </div>
-        </div>
-        
-
-        
       </Slider>
     </div>
   );
